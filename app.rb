63c7b93d5fef8b244925c7f985a610e27f794c04
Brandon Fonnemann <branjosef@gmail.com>
2020-10-07 10:27:59 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/reverse' do
     erb :reverse
   end
@@ -14,6 +15,7 @@ class App < Sinatra::Base
   get '/friends' do
     # Write your code here!
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
-     
+    erb :friends
   end
+
 end
